992f2d0683e2204624c8d6a2e042e20589dbb7f7
ShirishaGadamsetty <39770678+ShirishaGadamsetty@users.noreply.github.com>
2018-11-30 10:50:22 +0530

    nov 30 commit

--- logisticregON titanic.R
+++ logisticregON titanic.R
@@ -23,5 +23,30 @@ titanic <- titanic[-c(4,9,11)]
 #creating traing and testing sample
 #60% in traing data and 40% in test data
 
+passengerId <- train&passengerId
+index <- sample(1:nrow(titanic),size = 0.6*nrow(titanic))
+index
+ train <- titanic[index]
+ test1 <- titanic[-index]
 
-index <- sample(1:nrow)
+ head(titanic) 
+head(train)
+ 
+ #model building
+ model <- glm(survived ~ .,family = binomial(link = logit),data=titanic)
+
+ #model summary
+ summary(model)
+ train<- read.csv("c:/train.csv", header = TRUE)
+ test.survived <- data.frame(survived = rep("None", nrow(test)), test[,])
+ #scoring test data
+ prob <- predict(model,train,type = "response")
+head(prob)
+ #confusion matrix and accuracy
+pred <- rep(0,nrow(train))
+pred[prob> .5] =1
+table(pred,train$survived)
+
+
+
+ 
